d4c34c9ef040fbb41b1673e7e08e63323d092bf4
dhanraj <dhanrajp3531@gmail.com>
2020-03-26 17:30:27 +0530

    assignment 1

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector    : 'app-server',
+    templateUrl : './server.component.html',
+    // template    : `<div><p> New server here!!!...</p></div>`,
+    styleUrls   : ['./server.component.css']
+    // styles      : [`p { color:blueviolet}`]
+})
+export class ServerComponent{
+    name="default"
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector    : 'app-warn-alert',
+    templateUrl : './warning-alert.component.html',
+    styleUrls   : [ './warning-alert.component.css' ]
+})
+export class WarningAlertComponent {
+    output="You Have Been Warned"
+}
