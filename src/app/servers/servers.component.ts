7108a230e81c7e2c5440729e3f2bd2b097f5e926
dhanraj <dhanrajp3531@gmail.com>
2020-03-27 20:57:02 +0530

    finished second assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   // template:'<server></server><hr><server></server>',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+   maximum_allowed : Number = 10;
+   text_alignment : String = "center";
+   isDisabled : boolean = true;
+   server_status : String = "";
+   current_server_name : String = "";
+   car_name : String = "";
+   username : String = "";
+   flush_name(){
+     this.username = "";
+   }
+   create_server(){
+     this.server_status = "New instance created";
+   }
+   recce_server(event_data: Event){
+     this.current_server_name = (<HTMLInputElement>event_data.target).value;
+   }
+   constructor(){
+      setTimeout(()=>{
+        this.isDisabled = false;
+      },10000)
+   }
+   ngOnInit(){
+   }
 }
